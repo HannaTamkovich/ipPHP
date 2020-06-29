62e2393b5365183983fbf117558cac6961c61760
AnnBudai <anna.budaj@yandex.by>
2020-06-29 23:18:06 +0300

    images

--- js/view.contact.js
+++ js/view.contact.js
@@ -1,6 +1,3 @@
-document.getElementById('contactSuccess').hidden = true;
-document.getElementById('contactError').hidden = true;
-
 $(document).ready(function () {
     $("#contactForm").submit(function () {
         var form_data = $(this).serialize();
@@ -9,16 +6,12 @@ $(document).ready(function () {
             url: "https://a23f69d6695f.ngrok.io/orders",
             data: form_data,
             success: function () {
-                document.getElementById('contactSuccess').hidden = false;
                 document.getElementById('name').value = "";
                 document.getElementById('subject').value = "";
                 document.getElementById('email').value = "";
                 document.getElementById('message').value = "";
             },
             error: function () {
-                document.getElementById('contactError').hidden = false;
-
-                // alert('возникла ошибка');
             }
         });
         return false;
