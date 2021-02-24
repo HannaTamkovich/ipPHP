ca93318371de8e63fc8fde62afe1efba53bf5b87
Denis Tamkovich <tamkovichdenis@gmail.com>
2021-02-25 00:45:46 +0300

    Add banner message on home page

--- js/view.contact.js
+++ js/view.contact.js
@@ -16,4 +16,22 @@ $(document).ready(function () {
         });
         return false;
     });
+    $("#bannerForm").submit(function () {
+        let form_data = $(this).serialize();
+        form_data += '&email=';
+        console.log(form_data);
+        $.ajax({
+            type: "POST",
+            url: "https://ipbudaibot.ga/orders",
+            data: form_data,
+            success: function () {
+                document.getElementById('name').value = "";
+                document.getElementById('subject').value = "";
+                document.getElementById('message').value = "";
+            },
+            error: function () {
+            }
+        });
+        return false;
+    });
 });
